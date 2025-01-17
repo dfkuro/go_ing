9cd36fae3fcce1b257bd4db8a5852ebb3b50c625
dfkuro <izmirreffi@gmail.com>
2025-01-17 13:01:28 -0600

    fix on 08_arrays
    Bad use of len()

--- go_by_example/08_arrays/08_arrays.go
+++ go_by_example/08_arrays/08_arrays.go
@@ -24,8 +24,8 @@ func main() {
   fmt.Println("idx:", b)
 
   var twoD [2][3]int
-  for i := 0; i < len(twoD[0]); i++ {
-    for j := 0; j < len(twoD[1]); j++ {
+  for i := 0; i < 2; i++ {
+    for j := 0; j < 3; j++ {
       twoD[i][j] = i + j
     }
   }
