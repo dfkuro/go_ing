4e6707f0f7b0bcc197ba39681cfbad0f2e0f4168
dfkuro <izmirreffi@gmail.com>
2025-01-18 01:07:37 -0600

    Added comments

--- go_by_example/06_ifelse/06_ifelse.go
+++ go_by_example/06_ifelse/06_ifelse.go
@@ -3,30 +3,31 @@ package main
 import "fmt"
 
 func main() {
-  if 7%2 == 0 {
-    fmt.Println("7 is even")
-  } else {
-    fmt.Println("7 is odd")
-  }
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
 
-  if 8%4 == 0 {
-    fmt.Println("8 is divisible by 4")
-  }
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
 
-  if 8%2 == 0 || 7%2 == 0 {
-    fmt.Println("either 8 or 7 is even")
-  }
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 or 7 is even")
+	}
 
-  if num := 9; num < 0 {
-    fmt.Println("num is negative")
-  } else if num < 10 {
-    fmt.Println("has 1 digit") 
-  } else {
-    fmt.Println("has multiple digits")
-  }
+	// A statement can precede conditionals; any variables declared in this statement are
+	// available in the current and all subsequent branches
+	if num := 9; num < 0 {
+		fmt.Println("num is negative")
+	} else if num < 10 {
+		fmt.Println("has 1 digit")
+	} else {
+		fmt.Println("has multiple digits")
+	}
 
-  if 1 == 1 && 2 == 2 {
-    fmt.Println("both are equal")
-  }
-
+	if 1 == 1 && 2 == 2 {
+		fmt.Println("both are equal")
+	}
 }
