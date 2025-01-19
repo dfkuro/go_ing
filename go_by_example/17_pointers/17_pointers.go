4d321db89e41443097f0ec43f1e67f65f0799121
dfkuro <izmirreffi@gmail.com>
2025-01-19 17:09:04 -0600

    Added comment for 17_pointers.go

--- go_by_example/17_pointers/17_pointers.go
+++ go_by_example/17_pointers/17_pointers.go
@@ -1,13 +1,13 @@
 /**
 * Go supports pointers, allowing you to pass references to values
 *	and records within your program.
-*/
+ */
 package main
 
 import "fmt"
 
 // We’ll show how pointers work in contrast to values with 2
-// functions: zeroval and zeroptr. zeroval has an int parameter, 
+// functions: zeroval and zeroptr. zeroval has an int parameter,
 // so arguments will be passed to it by value.
 // zeroval will get a copy of ival distinct from the one in the calling function.
 func zeroval(ival int) {
@@ -18,7 +18,7 @@ func zeroval(ival int) {
 // an int pointer. The *iptr code in the function body then dereferences
 // the pointer from its memory address to the current value at that address.
 // Assigning a value to a dereferenced pointer changes the value at the referenced address.
-func zerptr(iptr *int) {
+func zeroptr(iptr *int) {
 	*iptr = 0
 }
 
